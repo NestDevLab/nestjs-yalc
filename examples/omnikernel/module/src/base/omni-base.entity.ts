35cd258405718e5a8e405225683ee7141f179ac6
Yehonal <yehonal.azeroth@gmail.com>
2026-04-15 17:45:42 +0000

    ci(examples): fix example builds in clean installs

--- examples/omnikernel/module/src/base/omni-base.entity.ts
+++ examples/omnikernel/module/src/base/omni-base.entity.ts
@@ -1,9 +1,13 @@
-import { EntityWithTimestamps } from '@nestjs-yalc/database/timestamp.entity';
-import { ObjectType } from '@nestjs/graphql';
-import { BaseEntity, PrimaryColumn } from 'typeorm';
+import { EntityWithTimestamps } from "@nestjs-yalc/database/timestamp.entity";
+import { ObjectType } from "@nestjs/graphql";
+import { BaseEntity, PrimaryColumn } from "typeorm";
 
 @ObjectType({ isAbstract: true })
 export abstract class OmniBaseEntity extends EntityWithTimestamps(BaseEntity) {
-  @PrimaryColumn('varchar', { name: 'guid', length: 36 })
+  @PrimaryColumn("varchar", { name: "guid", length: 36 })
   guid!: string;
+
+  declare createdAt: Date;
+
+  declare updatedAt: Date;
 }
--- examples/skeleton/app/apps/skeleton-app/src/users/users.errors.controller.ts
+++ examples/skeleton/app/apps/skeleton-app/src/users/users.errors.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { YalcEventService } from '@nestjs-yalc/event-manager';
+import { YalcEventService } from '@nestjs-yalc/event-manager/event.service';
 
 @Controller('users/errors')
 export class UsersErrorsController {
